926fef3f0132a62634ec7515f62cd5659cde1fef
Rafi Arya <rafiaryaramadhan2710@gmail.com>
2025-04-09 19:45:11 +0700

    Mendukung Bahasa Indonesia

--- app/src/main/java/com/rafiarya0114/temperatureconversion/ui/screen/MainScreen.kt
+++ app/src/main/java/com/rafiarya0114/temperatureconversion/ui/screen/MainScreen.kt
@@ -1,17 +1,30 @@
 package com.rafiarya0114.temperatureconversion.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rafiarya0114.temperatureconversion.R
 import com.rafiarya0114.temperatureconversion.ui.theme.TemperatureConversionTheme
 
@@ -37,12 +50,25 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android",
-        modifier = modifier
+    var inputValue by remember { mutableStateOf("") }
+    var inputError by remember { mutableStateOf(false) }
+    var selectedUnitForm by remember { mutableStateOf("Celcius") }
+    var selectedUnitTo by remember { mutableStateOf("Farenheit") }
+    var result by remember { mutableStateOf("") }
 
-    )
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
 }
+
+
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
